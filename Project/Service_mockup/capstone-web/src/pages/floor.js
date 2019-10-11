bf682688c8a47eb11f27a00b36109f469bf40b4d
Geunwoo Nam <alorse45@gmail.com>
2019-10-11 23:28:08 +0900

    floor 뷰 작업중

--- Project/Service_mockup/capstone-web/src/pages/floor.js
+++ Project/Service_mockup/capstone-web/src/pages/floor.js
@@ -1,13 +1,25 @@
 // a page for showing and editting the floor
+// https://ui.toast.com/tui-chart
+// TOAST UI를 사용해볼까?
+
+// 박스를 그려서 그 안에다가 배열을 출력한다.
+
+// 왼쪽: 행렬을 표시, 오른쪽: 각 물품의 재고 현황 표시
+
 import React from 'react'
+import { navigate } from 'gatsby';
 
 import SEO from '../components/seo'
 
 const floor = () => (
   <>
-    <SEO title="Page two" />
+    <SEO title="floor plan" />
     <h1>Floor</h1>
     <p>Welcome to the floor page.</p>
+
+    <Container maxWidth="500">
+    </Container>
+
   </>
 )
 
