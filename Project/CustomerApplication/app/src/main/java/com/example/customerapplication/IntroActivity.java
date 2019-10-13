fd44722220c7da9424f83896ade94460589fab86
sw00700 <sw00700@naver.com>
2019-10-13 21:39:30 +0900

    제안서, 일정표 수정

--- Project/CustomerApplication/app/src/main/java/com/example/customerapplication/IntroActivity.java
+++ Project/CustomerApplication/app/src/main/java/com/example/customerapplication/IntroActivity.java
@@ -1,30 +1,37 @@
 package com.example.customerapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 
 public class IntroActivity extends AppCompatActivity {
-    Handler handler = new Handler();
+
+
+    /*Handler handler = new Handler();
+
     Runnable r = new Runnable() {
         @Override
         public void run() {
-// 4초뒤에 다음화면(MainActivity)으로 넘어가기 Handler 사용
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent); // 다음화면으로 넘어가기
             finish(); // Activity 화면 제거
         }
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        checkInternetState();
     }
+    /*
     @Override
     protected void onResume() {
         super.onResume();
@@ -37,6 +44,43 @@ public class IntroActivity extends AppCompatActivity {
         super.onPause();
 // 화면을 벗어나면, handler 에 예약해놓은 작업을 취소하자
         handler.removeCallbacks(r); // 예약 취소
+    }*/
+
+    private void checkInternetState(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        assert cm != null;
+        if(!(cm.getActiveNetwork() != null && cm.getActiveNetworkInfo().isConnected())){
+            new AlertDialog.Builder(this)
+                    .setMessage(R.string.network_fail)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finishAffinity();
+                        }
+                    }).show();
+        }
+        else
+        {
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startTask();
+                }
+            }, 2000);
+        }
+    }
+
+    public void startTask() {
+        startMain();
+    }
+
+    public void startMain() {
+        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 
 }
---
+++ Project/CustomerApplication/app/src/main/java/com/example/customerapplication/PermissionActivity.java
@@ -0,0 +1,130 @@
+package com.example.customerapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermissionActivity extends AppCompatActivity {
+    private static final int PERMISSION_MULTI_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+
+        if (Build.VERSION.SDK_INT < 23) {
+            goIntroActivity();
+        } else {
+            if (checkAndRequestPermissions()) {
+                goIntroActivity();
+            }
+        }
+    }
+
+    private  boolean  checkAndRequestPermissions() {
+        String [] permissions = new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        for (String permission:permissions) {
+            if (ContextCompat.checkSelfPermission(this,permission )
+                    != PackageManager.PERMISSION_GRANTED){
+                listPermissionsNeeded.add(permission);
+            }
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    PERMISSION_MULTI_CODE);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (grantResults.length == 0) return;
+
+        switch (requestCode) {
+            case PERMISSION_MULTI_CODE:
+                checkPermissionResult(permissions, grantResults);
+
+                break;
+        }
+    }
+
+    private void checkPermissionResult(String[] permissions, int[] grantResults) {
+        boolean isAllGranted = true;
+
+        for (int i = 0; i < permissions.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                isAllGranted = false;
+            }
+        }
+
+        //권한이 부여되었다면
+        if (isAllGranted) {
+            goIntroActivity();
+
+            //권한이 부여되어 있지 않다면
+        } else {
+            showPermissionDialog();
+        }
+    }
+
+    private void goIntroActivity() {
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
+    private void showPermissionDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.permission_setting_title);
+        dialog.setMessage(R.string.permission_setting_message);
+        dialog.setPositiveButton(R.string.setting, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                Toast.makeText(PermissionActivity.this, R.string.permission_setting_restart, Toast.LENGTH_LONG).show();
+                PermissionActivity.this.finish();
+
+                goAppSettingActivity();
+            }
+        });
+        dialog.setNegativeButton(R.string.cancel,new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                PermissionActivity.this.finish();
+            }
+        });
+        dialog.show();
+    }
+
+    private void goAppSettingActivity() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+}
