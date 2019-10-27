e988673d00c4bdb0feafa482dc476e627419f3ca
hjchang <neropsys@gmail.com>
2019-10-27 18:36:02 +0900

    package-lock.json 삭제, 로그인 함수 구현

--- Project/Service_mockup/capstone-api/src/signup.js
+++ Project/Service_mockup/capstone-api/src/signup.js
@@ -18,7 +18,26 @@ app.post('/signup', async (req, res) => {
         res.status(403).send({ error: 'Something failed!' });
     }
 });
+app.post('/signin', async(req, res) => {
+    console.log({ 'req.body': req.body });
+    try {
+        const { id, password, } = req.body;
+        const result = await findMember(id, password);
+        console.log({ result });
+        res.send({ result });
+    } catch (err) {
+        console.log({ err });
+        res.status(403).send({ error: 'Something failed!' });
+    }
+});
 
+async function findMember(id, password){
+    const hash = await hashAsync(password, 10);
+    return await queryAsync(
+        'SELECT password FROM member_manager WHERE username=?',
+        [id]
+    );
+}
 async function insertMember(id, password, name, address, phoneNumber) {
     const hash = await hashAsync(password, 10);
     return await queryAsync(
