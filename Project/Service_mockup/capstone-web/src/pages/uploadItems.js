45c586875a67ec0039f625bcb79107a03bf458c3
Geunwoo Nam <alorse45@gmail.com>
2019-11-11 01:52:48 +0900

    물품 csv 업로드 추가

--- Project/Service_mockup/capstone-web/src/pages/uploadItems.js
+++ Project/Service_mockup/capstone-web/src/pages/uploadItems.js
@@ -1,25 +1,62 @@
 // csv로 된 상품 정보를 업로드하는 페이지
 // parse하고 그 결과를 db에 insert한다.
 
-import React from 'react'
+// https://github.com/vtex/react-csv-parse
+
+import React, { Component } from 'react'
 import Papa from 'papaparse'
-import CSVReader from 'react-csv-reader'
-import SEO from '../components/seo'
+import CsvParse from 'react-csv-reader'
+//import SEO from '../components/seo'
 
-const test1 = () => (
-  <>
-    <SEO title="Page two" />
-    <h1>Upload items</h1>
-    <p>This page is for uploading items by uploading a csv file.</p>
-    {/* <CSVReader
-        cssClass="csv-reader-input"
-        label="Select CSV with secret Death Star statistics"
-        onFileLoaded={this.handleForce}
-        onError={this.handleDarkSideForce}
-        inputId="ObiWan"
-        inputStyle={{color: 'red'}}
-      /> */}
-  </>
-)
+const keys = [
+  "header1",
+  "header2",
+  "header3",
+  "header4",
+  "header5",
+]
 
-export default test1;
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  onChangeHandler = event => {
+    console.log({"event":event.target.files[0]});
+  }
+  render() {
+    return (
+      <>
+        <h1>Upload items</h1>
+        <p>This page is for uploading items by uploading a csv file.</p>
+
+        {/* <input
+          type="file"
+          ref={(input) => { 
+            this.fileInput = input;
+          console.log(input);}}
+          name="file"
+          icon='file text outline'
+          iconPosition='left'
+          label='Upload CSV'
+          labelPosition='right'
+          placeholder='UploadCSV...'
+          onChange={this.onChangeHandler}
+        /> */}
+
+        
+
+
+        <CsvParse
+          keys={keys}
+          onDataUploaded={this.handleData}
+          onError={this.handleError}
+          render={onChange => <input type="file" onChange={onChange} />}
+        />
+      </>
+    );
+  }
+}
+
+export default Form;
