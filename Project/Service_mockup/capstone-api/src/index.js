563b489deea34da9a30793b7b0b6ee5e21205d20
Geunwoo Nam <alorse45@gmail.com>
2019-11-10 18:31:21 +0900

    minor change

--- Project/Service_mockup/capstone-api/src/index.js
+++ Project/Service_mockup/capstone-api/src/index.js
@@ -70,7 +70,7 @@ app.get('/item_quantity', async (req, res) => {
         console.log({ err });
         res.status(403).send({ error: 'Something failed! '});
     }
-);
+});
 
 app.get('/dashboard_quantity', async (req, res) => {
     console.log({ 'req.body': req.body });
