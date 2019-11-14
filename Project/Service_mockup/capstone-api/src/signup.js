5b76ee2aa3020f779846415afac0e731cb6ce331
neropsys <neropsys@gmail.com>
2019-11-14 17:42:35 +0900

    로그인 성공시 유저id 리턴 및 쿠키에 저장

--- Project/Service_mockup/capstone-api/src/signup.js
+++ Project/Service_mockup/capstone-api/src/signup.js
@@ -11,36 +11,32 @@ app.post('/signup', async (req, res) => {
     try {
         const { id, password, name, address, phoneNumber } = req.body;
         const result = await insertMember(id, password, name, address, phoneNumber);
-        console.log({ result });
         res.send({ result });
     } catch (err) {
         console.log({ err });
         res.status(403).send({ error: 'Something failed!' });
     }
 });
-app.post('/signin', async(req, res) => {
+app.post('/signin', async (req, res) => {
     console.log({ 'req.body': req.body });
     try {
         const { id, password, } = req.body;
         const result = await findMember(id, password);
-
-        console.log({ result });
-        res.send({ result });
+        res.send({ "id": result });
     } catch (err) {
         res.status(403).send({ error: 'Something failed!' });
     }
 });
 
-async function findMember(id, password){
+async function findMember(id, password) {
     let result = await queryAsync(
-        'SELECT password FROM member_manager WHERE username=?',
+        'SELECT password, idmember_manager FROM member_manager WHERE username=?',
         [id]
     );
-    
+
     let compResult = await compareAsync(password, result[0].password);
-    console.log(compResult);
-    if(compResult===true){
-        return JSON.stringify("OK");
+    if (compResult === true) {
+        return result[0].idmember_manager;
     }
     else throw null;
 }
