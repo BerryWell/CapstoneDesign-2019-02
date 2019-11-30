ff8dec47bf7f8c03ca22dae098eb4a6ab14a278a
hjchang <neropsys@gmail.com>
2019-11-30 22:12:25 +0900

    매장 id 주면 매장의 아이템이 쇼핑리스트에 몇번 등록되었는지 리턴해주는 api

--- Project/Service_mockup/capstone-api/src/android_market.js
+++ Project/Service_mockup/capstone-api/src/android_market.js
@@ -1,20 +1,20 @@
 
 const { app } = require('./app');
 const { queryAsync } = require('./connection');
-const { promisify } = require('util');
 const sqlConfig = require('./config');
-
+const {promisify} = require('util');
 const redis = require('redis');
 
 let client = redis.createClient(6379, sqlConfig.development.redis.host);
 client.auth(sqlConfig.development.redis.password);
+const getAsync = promisify(client.get.bind(client));
 client.on('connect', function() {
     console.log('Redis client connected');
 });
 client.on('error', function (err) {
     console.log('Something went wrong ' + err);
 });
-app.post('/itemstat/popularity', async(req, res) => {
+app.post('/itemstat/popularity', async (req, res) => {
     let itemList = req.body;
     console.log(itemList);
     itemList.map(element => {
@@ -28,7 +28,26 @@ app.post('/itemstat/popularity', async(req, res) => {
     });
     res.status(200).send({OK:true});
 });
-
+app.get('/markets/popularity/:id', async (req, res) => {
+    try{
+        let resultList = await queryAsync('SELECT item.name, item.iditem FROM item, floor WHERE floor.mall_idmall = ? AND floor.idfloor = item.floor_idfloor', [req.params.id]);
+       console.log({_resultList:resultList});
+        let returnList = [];
+        for(let i=0; i < resultList.length; i++){
+            let element = resultList[i];
+            let ret = await getAsync(element.iditem);
+            console.log(ret);
+            if(ret){
+                returnList.push({name:element.name, count:ret});
+            }
+        }
+        console.log({_returnList:returnList});
+        res.status(200).send(returnList);
+    } catch (err) {
+        console.log({ err });
+        res.status(403).send({ error: 'Something failed!' });
+    }
+});
 app.get('/markets', async (req, res) => {
     let lat = req.query.lat;
     let long = req.query.long;
